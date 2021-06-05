97856033a4c51e8b3d2d5460ce68051659177a78
Monika Pawar <monika.pawar.we@gmail.com>
2021-06-05 12:44:53 +0530

    footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,18 @@ $(window).scroll(function(){
 $( function() {
     $( "#accordion" ).accordion();
   } );
+  // $(".accordion-demo").accordionjs({
+
+  //   // The section open on first init.
+  //   activeIndex : 1,     
+  
+  //   // Closeable section.
+  //   closeAble: false, 
+  
+  //   // Close other sections.
+  //   closeOther  : true,  
+  
+  //   // the speed of slide animation.
+  //   slideSpeed: 200 
+    
+  // });
